0b0211bdaf3dd60060787a3cf1e881a983323cff
hüseyin öztaş <huseyinooztas@gmail.com>
2023-12-14 13:54:00 +0300

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,471 @@
+import logo from '../images/logo.png'
+import ecommerce from '../images/Ecommerce.png';
+import courses from '../images/courses.png';
+import todos from '../images/todoApp.png'
+import friendsapp from '../images/friendsapp.png'
+import github from '../images/githubfinder.png';
+import nature from '../images/naturetours.png';
+import restourant from '../images/restourant.png';
+import tindog from '../images/tindog.png';
+
+
+export const Bio = {
+  name: "Hüseyin Öztaş",
+  roles: [
+    "Front End Developer",
+    "Sales Representative",
+    "Dancer",
+    "Programmer",
+  ],
+  description:
+    "I have been a sales representative for years and I recieved product management training last year. I have been working to become a Front End Developer recently and have taken several courses on it.I developed an application in MERN Stack format.I did a lot of things, you can see them all on my github profile.",
+  github: "https://github.com/huseyinoztas",
+  resume:
+    "https://drive.google.com/file/d/1IfF6HsSC1LZGIj5HKDcFoO43LnxpnqWy/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/h%C3%BCseyin-%C3%B6-674873b5/",
+  twitter: "https://twitter.com",
+  insta: "https://www.instagram.com/huseyinn236",
+  facebook: "https://www.facebook.com",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Material UI",
+        image:
+          "https://mui.com/static/logo.png",
+          link:"www.google.com"
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://adware-technologies.s3.amazonaws.com/uploads/technology/thumbnail/20/express-js.png",
+      },
+      {
+        name: "Graph Ql",
+        image: "https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+      {
+        name: "Adobe XD",
+        image:
+          "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://scontent.fesb9-1.fna.fbcdn.net/v/t39.30808-1/400854449_655620063285410_5630615764470196434_n.jpg?stp=cp0_dst-jpg_e15_p320x320_q65&_nc_cat=110&ccb=1-7&_nc_sid=4da83f&_nc_ohc=4vddRBc_7uAAX-9WZEd&_nc_ht=scontent.fesb9-1.fna&oh=00_AfDxyi6O5A8mNTPplUe83ZZLMXPLFt7kHX0UpMemp3QcAw&oe=657B5E45",
+    role: "Sales Representative",
+    company: "Basel Ilac",
+    date: "March 2020 to Present",
+    desc: "Sales and Marketing Pharmaceutical products to Pharmacys / Doctors. Monitoring and Analyzing Sales data through regular visits and conversation with Customer.",
+    skills: [
+      "Sales",
+      "Communication",
+      "Flexibility",
+      "Time Management",
+      "Teamwork",
+      "CRM",
+    ],
+    
+  },
+  {
+    id: 1,
+    img: "https://luminahealthcaretr.com/wp-content/uploads/2023/03/cropped-lumina_healthcare_logo_son_onayli-1-2048x819.png",
+    role: "Wordpress Dev",
+    company: "Lumina Healthcare",
+    date: "March 2023",
+    desc: "One-time job of making a corporate website. And the counseling continues.",
+    skills: [
+      "Wordpress",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "https://luminahealthcaretr.com/",
+  },
+  {
+    id: 2,
+    img: logo,
+    role: "Sales Representative",
+    company: "Premium Pharma",
+    date: "April 2018-January 2019",
+    desc: "Sales and Marketing Pharmaceutical products to Pharmacys / Doctors. Monitoring and Analyzing Sales data through regular visits and conversation with Customer.",
+    skills: [
+      "Sales",
+      "Communication",
+      "Flexibility",
+      "Time Management",
+      "Teamwork",
+      "CRM",
+    ],
+  },
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/I/41VX-2tA8aL._AC_UF1000,1000_QL80_.jpg",
+    role: "Sales Representative ",
+    company: "Newita",
+    date: "November 2017-April 2018",
+    desc: "Medical / Pharmaceutical promotions and sales in Bursa, Balıkesir and Çanakkale. Made redundant after the Company went into difficulties.",
+    skills: [
+      "Sales",
+      "Communication",
+      "Flexibility",
+      "Time Management",
+      "Teamwork",
+      "OTC",
+      "CRM",
+    ],
+  },
+  {
+    id: 4,
+    img: "https://e7.pngegg.com/pngimages/10/579/png-clipart-ballroom-dance-ballroom-tango-graphics-salsa-fictional-character-shoe.png",
+    role: "Tango Instructor",
+    company: "Student CLub in University",
+    date: "2014 to present",
+    desc: "I started tango at university and later continued by giving lessons.",
+    skills: [
+      "Leadership",
+      "Teaching",
+      "Teamwork",
+      "Flexibility",
+      "Time Management",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.uludag.edu.tr/dosyalar/saglikkultur/2015-PDF/menu_img/uulogo_600.png",
+    school: "Bursa Uludag University",
+    date: "Sep 2011 - July 2015",
+    grade: "2,34",
+    desc: "I graduated from Uludağ University in 2011 with a degree in Public Administration. My academic journey at the Department of Public Administration equipped me with a comprehensive understanding of governance, public policy, and organizational management. This educational background has been instrumental in shaping my professional perspective and fueling my passion for contributing to effective public service. Since then, I have continued to build upon the foundation laid during my undergraduate studies through practical experiences and ongoing learning opportunities",
+    degree: "Public Management",
+  },
+];
+
+
+
+export const projects = [
+  {
+    id: 0,
+    title: "E-commerce Website",
+    date: "September 2023",
+    description:
+      "Explore the synergy of cutting-edge technologies at our Redux-Tailwind powered website. Merging the robust functionality of Redux with the sleek styling of Tailwind CSS, I've crafted an immersive digital experience. ",
+    image:ecommerce,
+    tags: [
+      "React Js",
+      "Redux",
+      "Tailwind",
+      "Fetch API",
+    ],
+    category: "web app",
+    github: "https://github.com/huseyinoztas/eCommerceWebsite",
+    webapp: "https://ecommercerwebsite.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "Courses",
+    date: "January 2023",
+    description:
+      "Developed that full stack application with React Js, MongoDB, Node Js, Express Js, and GraphQL. Immerse yourself in a transformative learning experience where you can seamlessly add, edit, and remove courses, instructors, and memberships. Our platform seamlessly integrates the power of React for dynamic user interfaces, MongoDB for robust data storage, and Node Js with Express Js for a scalable and efficient backend. ",
+    image:courses,
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "GraphQL"],
+    category: "web app",
+    github: "https://github.com/huseyinoztas/CoursesFrontend",
+    webapp: "https://coursesfrontend.onrender.com/",
+  },
+  {
+    id: 2,
+    title: "Todo App",
+    date: "January 2023",
+    description:
+      "This is a fullstack app and developed with MERNG. This seamlessly integrated stack brings together the power of MongoDB for flexible data storage, Express.js and Node.js for a robust backend, React for a dynamic and responsive frontend, and GraphQL for efficient data queries.",
+    image:todos,
+    tags: [
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "GraphQL",
+    ],
+    category: "web app",
+    github: "https://github.com/huseyinoztas/todo-merng-frontend",
+    webapp: "https://todo-app-pl8o.onrender.com/",
+  },
+  {
+    id: 3,
+    title: "Friends App",
+    date: "December 2022",
+    description:
+      "I made this project using material ui.  And ı think its really cool this library. HTML, CSS, Javascript, Material UI. I used many properties this project. You can reach the source code in my github page.",
+    image:friendsapp,
+    tags: ["HTML", "CSS", "ReactJs", "Javascript", "Material UI"],
+    category: "web app",
+    github: "https://github.com/huseyinoztas/friendsapp",
+    webapp: "https://friendss-app.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "Github Finder",
+    date: "December 2022",
+    description:
+      "I used many hooks in this project and I used github API with context api. You can search users and you can see their profile.",
+    image:github,
+    tags: ["HTML", "CSS", "ReactJs", "Javascript","Context API","Axios"],
+    category: "web app",
+    github: "https://github.com/huseyinoztas/Githubfinder",
+    webapp: "https://git-hub-finderr.netlify.app/",
+  },
+  {
+    id: 5,
+    title: "Nature Tours",
+    date: "November 2022",
+    description:
+      "In this project I used just html and css. But not simple project because made with SCSS and many properties. If you wonder you can click the link below. ",
+    image:nature,
+    tags: ["HTML", "CSS","SASS","SCSS" ],
+    category: "web app",
+    github: "https://github.com/huseyinoztas/nature-tours",
+    webapp: "https://huseyinoztas.github.io/nature-tours/",
+  },
+  {
+    id: 6,
+    title: "Restaurant Site",
+    date: "February 2023",
+    description:
+      "I made this with react to be an example restaurant website.",
+    image:restourant,
+    tags: ["React Js", "Javascript", "HTML","CSS", "Reactstrap"],
+    category: "web app",
+    github: "https://github.com/huseyinoztas/RestaurantSite",
+    webapp: "https://restaurant-sitee.netlify.app/",
+  },
+  {
+    id: 7,
+    title: "TinDog",
+    date: "September 2022",
+    description:
+      " This is my firs project in this field and I was enjoy when ı did this. I used bootstrap in this project..",
+    image:tindog,
+    tags: ["HTMl","CSS","Bootstrap"],
+    category: "web app",
+    github: "https://github.com/huseyinoztas/TinDog",
+    webapp: "https://huseyinoztas.github.io/TinDog/",
+  },
+];
+
+export const certificate = [
+  {
+    id: 0,
+    title: "The Complete 2023 Web Development Bootcamp",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Explore the synergy of cutting-edge technologies at our Redux-Tailwind powered website. Merging the robust functionality of Redux with the sleek styling of Tailwind CSS, I've crafted an immersive digital experience. ",
+    image:"https://udemy-certificate.s3.amazonaws.com/image/UC-903731f5-4dc4-4a82-a63e-9a7f28ba36ab.jpg?v=1673644165000",
+    tags: [
+      "HTML",
+      "CSS",
+      "Flexbox",
+      "Javascript",
+      "JQuery",
+      "NodeJs",
+      "ExpressJs",
+      "SQL",
+      "PostgreSQL",
+      "ReactJs",
+
+    ],
+    github: "https://www.udemy.com/course/the-complete-web-development-bootcamp/",
+    webapp: "https://www.udemy.com/certificate/UC-903731f5-4dc4-4a82-a63e-9a7f28ba36ab/",
+  },
+  {
+    id: 1,
+    title: "Courses",
+    date: "Apr 2023 - May 2023",
+    description:
+      "Developed that full stack application with React Js, MongoDB, Node Js, Express Js, and GraphQL. Immerse yourself in a transformative learning experience where you can seamlessly add, edit, and remove courses, instructors, and memberships. Our platform seamlessly integrates the power of React for dynamic user interfaces, MongoDB for robust data storage, and Node Js with Express Js for a scalable and efficient backend. ",
+    image:courses,
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "GraphQL"],
+    category: "web app",
+    github: "https://github.com/huseyinoztas/CoursesFrontend",
+    webapp: "https://coursesfrontend.onrender.com/",
+  },
+  {
+    id: 2,
+    title: "Todo App",
+    date: "Oct 2022 - Present",
+    description:
+      "This is a fullstack app and developed with MERNG. This seamlessly integrated stack brings together the power of MongoDB for flexible data storage, Express.js and Node.js for a robust backend, React for a dynamic and responsive frontend, and GraphQL for efficient data queries.",
+    image:todos,
+    tags: [
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+      "GraphQL",
+    ],
+    category: "web app",
+    github: "https://github.com/huseyinoztas/todo-merng-frontend",
+    webapp: "https://todo-app-pl8o.onrender.com/",
+  },
+  {
+    id: 3,
+    title: "Friends App",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "I made this project using material ui.  And ı think its really cool this library. HTML, CSS, Javascript, Material UI. I used many properties this project. You can reach the source code in my github page.",
+    image:friendsapp,
+    tags: ["HTML", "CSS", "ReactJs", "Javascript", "Material UI"],
+    category: "web app",
+    github: "https://github.com/huseyinoztas/friendsapp",
+    webapp: "https://friendss-app.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "Github Finder",
+    date: "Dec 2021 - Apr 2022",
+    description:
+      "I used many hooks in this project and I used github API with context api. You can search users and you can see their profile.",
+    image:github,
+    tags: ["HTML", "CSS", "ReactJs", "Javascript","Context API","Axios"],
+    category: "web app",
+    github: "https://github.com/huseyinoztas/Githubfinder",
+    webapp: "https://git-hub-finderr.netlify.app/",
+  },
+  {
+    id: 5,
+    title: "Nature Tours",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "In this project I used just html and css. But not simple project because made with SCSS and many properties. If you wonder you can click the link below. ",
+    image:nature,
+    tags: ["HTML", "CSS","SASS","SCSS" ],
+    category: "web app",
+    github: "https://github.com/huseyinoztas/nature-tours",
+    webapp: "https://huseyinoztas.github.io/nature-tours/",
+  },
+  {
+    id: 6,
+    title: "Restaurant Site",
+    date: "Jul 2021",
+    description:
+      "I made this with react to be an example restaurant website.",
+    image:restourant,
+    tags: ["React Js", "Javascript", "HTML","CSS", "Reactstrap"],
+    category: "web app",
+    github: "https://github.com/huseyinoztas/RestaurantSite",
+    webapp: "https://restaurant-sitee.netlify.app/",
+  },
+  {
+    id: 7,
+    title: "TinDog",
+    date: "Jun 2021",
+    description:
+      " This is my firs project in this field and I was enjoy when ı did this. I used bootstrap in this project..",
+    image:tindog,
+    tags: ["HTMl","CSS","Bootstrap"],
+    category: "web app",
+    github: "https://github.com/huseyinoztas/TinDog",
+    webapp: "https://huseyinoztas.github.io/TinDog/",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
